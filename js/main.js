86540565d4a35c969846d664cc266455615a39a6
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-01-16 21:01:37 -0300

    Inicio do desenvolvimento da automação da ficha

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const personagem = document.querySelectorAll('#input-personagem');
+const atributos = document.querySelectorAll('#atributo');
+
+const For = parseInt(atributos[0].innerHTML)
+const Des = parseInt(atributos[1].innerHTML)
+const Con = parseInt(atributos[2].innerHTML)
+const Int = parseInt(atributos[3].innerHTML)
+const Sab = parseInt(atributos[4].innerHTML)
+const Car = parseInt(atributos[5].innerHTML)
+
+
