418b233e8bd964dc90dd5debb33c344b68adde4c
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-01-19 21:58:22 -0300

    Criação do Header e alterações em alguns elementos

---
+++ js/criacaoDeFicha.js
@@ -0,0 +1,18 @@
+const personagem = document.querySelectorAll('#input-personagem');
+const atributos = document.querySelectorAll('#atributo_input');
+
+const nomePersonagem = personagem[0];
+
+console.log(nomePersonagem.value);
+
+//------------ ATRIBUTOS ---------------------------
+const For = parseInt(atributos[0].value)
+const Des = parseInt(atributos[1].value)
+const Con = parseInt(atributos[2].value)
+const Int = parseInt(atributos[3].value)
+const Sab = parseInt(atributos[4].value)
+const Car = parseInt(atributos[5].value)
+
+console.log(For);
+
+
