bd8810ae9f31fb084dd6cdc8dc704ec32cd2f64d
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-02-10 20:06:06 -0300

    Add raca.js com as habilidades de cada raça

---
+++ js/raca.js
@@ -0,0 +1,29 @@
+export const racas = [{
+    nome: "Humano",
+    id: "1",
+    modAtribut: `+1 em Três Atributos Diferentes. Filhos
+    de Valkaria, Deusa da Ambição, humanos podem se
+    destacar em qualquer caminho que escolherem.`,
+    habilidade1: `Versátil. Você se torna treinado em duas pe￾
+    rícias a sua escolha (não precisam ser da sua classe).
+    Você pode trocar uma dessas perícias por um poder
+    geral a sua escolha.`
+},
+{
+    nome: "Anão",
+    id: "2",
+    modAtribut: `Constituição +2, Sabedoria +1, Destreza -1.`,
+    habilidade1: `Conhecimento das Rochas. Você recebe visão no escuro e +2 em testes de Percepção e Sobrevivência realizados no subterrâneo.`,
+    habilidade2: `Devagar e Sempre. Seu deslocamento é 6m (em vez de 9m). Porém, seu deslocamento não é reduzido por uso de armadura ou excesso de carga.`,
+    habilidade3: `Duro como Pedra. Você recebe +3 pontos de vida no 1º nível e +1 por nível seguinte.`,
+    habilidade4: `Tradição de Heredrimm. Você é perito nas armas tradicionais anãs, seja por ter treinado com elas, seja por usá-las como ferramentas de ofício. Para você, todos os machados, martelos, marretas e picaretas são armas simples. Você recebe +2 em ataques com essas armas.`
+},
+{
+    nome: "Anão",
+    id: "2",
+    modAtribut: `Sabedoria +2, Destreza +1, Inteligência -1.`,
+    habilidade1: `Amiga das Plantas. Você pode lançar a magia Controlar Plantas (atributo-chave Sabedoria). Caso aprenda novamente essa magia, seu custo diminui em -1 PM`,
+    habilidade2: `Armadura de Allihanna. Você pode gastar uma ação de movimento e 1 PM para transformar sua pele em casca de árvore, recebendo +2 na Defesa até o fim da cena.`,
+    habilidade3: `Duro como Pedra. Você recebe +3 pontos de vida no 1º nível e +1 por nível seguinte.`,
+    habilidade4: `Tradição de Heredrimm. Você é perito nas armas tradicionais anãs, seja por ter treinado com elas, seja por usá-las como ferramentas de ofício. Para você, todos os machados, martelos, marretas e picaretas são armas simples. Você recebe +2 em ataques com essas armas.`
+}]
